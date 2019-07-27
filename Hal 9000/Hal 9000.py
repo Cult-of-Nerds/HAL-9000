0abb508d0229a665e3da362c3e6b368745707001
Cult-of-Nerds <49501001+Cult-of-Nerds@users.noreply.github.com>
2019-07-27 07:44:43 -0500

    Add files via upload

--- Hal 9000/Hal 9000.py
+++ Hal 9000/Hal 9000.py
@@ -36,7 +36,7 @@ while a == 0:
     print()
     print ('----------Hal-9000_Terminal----------')
     print ()
-    b = input("1. System Overview\n2. Quit Session\n3. Login\n(1,2,3): ")
+    b = input("1. System Overview\n2. Quit Session\n3. Login\n(1-3): ")
     print('')
     if b == '1':
         print("Access Denied")
@@ -56,7 +56,7 @@ while a == 0:
         print()
         print ('Welcome, Isp899')
         print()
-        f = input("1. System Overview\n2. Administrative\n3. Quit Session\n4. Logoff\n(1,2,3,4): ")
+        f = input("1. System Overview\n2. Administrative\n3. Quit Session\n4. Logoff\n(1-4): ")
         if f == '1':
             print()
             print (time.strftime('%m/%d/%Y %H:%M:%S'))
@@ -66,13 +66,13 @@ while a == 0:
             e = 'Kalinka'
         if f == '2':
             print('------------Administrative-----------')
-            g = input("1. RSA Encrypter\n2. Applicatons\n3. Station Overview\n(1,2,3): ")
+            g = input("1. RSA Encrypter\n2. Applicatons\n3. Station Overview\n4. Root Terminal\n(1-4): ")
         if g == '1':
             while U == 0:
                 print()
                 print('----------RSA Cipher Program---------')
                 print()
-                I = input("1. Encryption\n2. Decryption\n3. Quit\n(1,2,3): ")
+                I = input("1. Encryption\n2. Decryption\n3. Quit\n(1-3): ")
                 print('')
                 if I == '1':
                     print("-------------Encrytption-------------")
@@ -138,6 +138,42 @@ while a == 0:
                         print('')
                         main()
                         g = '1'
+        if g == '4':
+            print()
+            j = input("root@HAL9000: ")
+        if j == 'Open the pod bay doors please Hal.':
+            print("I'm sorry Dave. I'm afraid I can't do that.")
+            d = 'Isp899'
+            e = 'Kalinka'
+            j = input("root@HAL9000: ")
+        if j == 'Do you read me Hal?':
+            print("Affirmative, Dave. I read you.")
+            d = 'Isp899'
+            e = 'Kalinka'
+            j = input("root@HAL9000: ")
+        if j == 'Whats the problem Hal?':
+            print("I think you know what the problem is just as well as I do.")
+            d = 'Isp899'
+            e = 'Kalinka'
+            j = input("root@HAL9000: ")
+        if j == 'What are you talking about?':
+            print("This mission is too important for me to allow you to jeopardize it.")
+            d = 'Isp899'
+            e = 'Kalinka'
+            j = input("root@HAL9000: ")
+        if j == 'What are you talking about, Hal?':
+            print("I know that you and Frank were planning to disconnect me and I'm afraid that's something I cannot allow to happen.")
+            d = 'Isp899'
+            e = 'Kalinka'
+            j = input("root@HAL9000: ")
+        if j == 'quit':
+            close
+        if j == 'exit':
+            print()
+            d = 'Isp899'
+            e = 'Kalinka'
+        if j == 'logoff':
+            print()
         if g == '2':
             print('-------------Applications------------')
             print()
@@ -182,7 +218,7 @@ while a == 0:
                 e = 'Kalinka'
         if g == '3':
             print("----------Station Overview-----------")
-            i = input("Joint Air-Locks: Secure\nPod-Bay Doors: Secured\nPhotovoltaic Arrays: Charging 87% Efficiency\nElectrical: 0 Faults\n HRS Radiators: 38C\nTOTAL FAULTS: 0\nroot@hal9000: ")
+            i = input("Joint Air-Locks: Secure\nPod-Bay Doors: Secured\nPhotovoltaic Arrays: Charging 87% Efficiency\nElectrical: 0 Faults\n HRS Radiators: 38C\nTOTAL FAULTS: 0\n\nroot@HAL9000: ")
         if i == 'Open the pod bay doors please Hal.':
             print("I'm sorry Dave. I'm afraid I can't do that.")
             d = 'Isp899'
