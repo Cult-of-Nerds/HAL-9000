8e7611acd466db38c53198d026c75319ed14f6be
Cult-of-Nerds <49501001+Cult-of-Nerds@users.noreply.github.com>
2019-07-28 11:53:40 -0500

    Add files via upload

--- Hal 9000/Hal 9000.py
+++ Hal 9000/Hal 9000.py
@@ -19,6 +19,9 @@ k = 0
 l = 0
 m = 0
 n = 0
+Isp899 = "Isp899"
+Kalinka = "Kalinka"
+Guest = "Guest"
 P = 7793
 Q = 7789
 N = P * Q
@@ -32,10 +35,14 @@ G = 0
 import os
 import time
 import subprocess
+import webbrowser
+import random
+import datetime
+from time import gmtime, strftime
 while a == 0:
     print()
-    print ('----------Hal-9000_Terminal----------')
-    print ()
+    print('----------Hal-9000 Terminal----------')
+    print(time.strftime('----%m/%d/%Y-----------%H:%M:%S----'))
     b = input("1. System Overview\n2. Quit Session\n3. Login\n(1-3): ")
     print('')
     if b == '1':
@@ -59,15 +66,22 @@ while a == 0:
         f = input("1. System Overview\n2. Administrative\n3. Quit Session\n4. Logoff\n(1-4): ")
         if f == '1':
             print()
-            print (time.strftime('%m/%d/%Y %H:%M:%S'))
+            print(time.strftime('%m/%d/%Y %H:%M:%S'))
             print('All Systems Ok')
             print()
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if f == '2':
             print('------------Administrative-----------')
             g = input("1. RSA Encrypter\n2. Applicatons\n3. Station Overview\n4. Root Terminal\n(1-4): ")
         if g == '1':
+#*********
+# -*- Created by Vance and Ian
+# -*- https://github.com/Cult-of-Nerds
+# -*- https://www.youtube.com/channel/UCCxkqK3cD49RXKsMSLGrppQ
+# -*- This work is licensed under the GNU General Public License v3.0
+#*********
+#RSA Encryptor
             while U == 0:
                 print()
                 print('----------RSA Cipher Program---------')
@@ -124,7 +138,7 @@ while a == 0:
                     I.close()
                     input()
                 if I != '1' and I != '2' and I != '3':
-                    print("ERROR 3: ONLY CHOOSE 1,2,3")
+                    print("ERROR 3: ONLY CHOOSE 1-3")
                 input()
                 U = 0
                 if I == '3':
@@ -132,8 +146,8 @@ while a == 0:
                     G = input("Are you sure you want to Exit? . . . Hal 9000 is a lot worse.\n(y,n): ")
                     if G == 'y':
                         print('')
-                        d = 'Isp899'
-                        e = 'Kalinka'
+                        d = Isp899
+                        e = Kalinka
                     if G == 'n':
                         print('')
                         main()
@@ -143,35 +157,74 @@ while a == 0:
             j = input("root@HAL9000: ")
         if j == 'Open the pod bay doors please Hal.':
             print("I'm sorry Dave. I'm afraid I can't do that.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
             j = input("root@HAL9000: ")
         if j == 'Do you read me Hal?':
             print("Affirmative, Dave. I read you.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
             j = input("root@HAL9000: ")
         if j == 'Whats the problem Hal?':
             print("I think you know what the problem is just as well as I do.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
             j = input("root@HAL9000: ")
         if j == 'What are you talking about?':
             print("This mission is too important for me to allow you to jeopardize it.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
             j = input("root@HAL9000: ")
         if j == 'What are you talking about, Hal?':
             print("I know that you and Frank were planning to disconnect me and I'm afraid that's something I cannot allow to happen.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
             j = input("root@HAL9000: ")
+        if j == 'Date':
+            print (time.strftime('%m/%d/%Y %H:%M:%S'))
+            d = Isp899
+            e = Kalinka
+            j = input("root@HAL9000: ")
+        if j == 'Weather':
+            webbrowser.open('https://weather.com/weather/today')
+            d = Isp899
+            e = Kalinka
+        if j == 'Search':
+            print('What to search?')
+            findWeb = input()
+            if findWeb != ('STOP'):
+                webbrowser.open('https://www.google.com/search?ei=yDG3XImiBKaAtgWyyLi4Bw&q='+ findWeb +'&oq=hello+def&gs_l=psy-ab.1.0.0l10.3811.4542..5781...0.0..0.266.773.0j2j2......0....1..gws-wiz.......0i71j0i67j0i131.YtTXdO35_O4')
+                d = Isp899
+                e = Kalinka
+        if j == 'help':
+            print("-----------I need somebody-----------")
+            print("For more information on a specific command, type HELP <<command-name>>")
+            print()
+            print("Date         Displays the date and time.")
+            print()
+            print("Search       Search the internet.")
+            print()
+            print("Weather      Displays the current weather.")
+            d = Isp899
+            e = Kalinka
+        if j == 'help Date':
+            print("Displays the date and time.(m/d/y H:M:S)")
+            d = Isp899
+            e = Kalinka
+        if j == 'help Search':
+            print("Searches the internet using the default web browser.")
+            d = Isp899
+            e = Kalinka
+        if j == 'help Weather':
+            print("Displays the weather in the default web browser.(weather.com)")
+            d = Isp899
+            e = Kalinka
         if j == 'quit':
             close
         if j == 'exit':
             print()
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if j == 'logoff':
             print()
         if g == '2':
@@ -182,63 +235,63 @@ while a == 0:
                 subprocess.Popen(['C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'])
             if h == '2':
                 subprocess.Popen(['C:\Program Files (x86)\Steam\Steam.exe'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '3':
                 subprocess.Popen(['C:/Users/ips89/AppData/Local/Discord/Update.exe'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '4':
                 subprocess.Popen(['C:/Users/ips89/AppData/Roaming/Spotify/Spotify.exe'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '5':
                 subprocess.Popen(['"C:/Program Files/Microsoft Office/root/Office16/WINWORD.EXE"'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '6':
                 subprocess.Popen(['"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '7':
                 subprocess.Popen(['C:\Windows\SysWOW64\explorer.exe'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '8':
                 subprocess.Popen(['"C:\Program Files\Adobe\Adobe Premiere Elements 2019\Elements Home\Adobe Premiere Elements 2019.exe"'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '9':
                 subprocess.Popen(['C:\Program Files\Mozilla Firefox\firefox.exe'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
             if h == '10':
                 subprocess.Popen(['C:\Program Files\Mozilla Firefox\firefox.exe'])
-                d = 'Isp899'
-                e = 'Kalinka'
+                d = Isp899
+                e = Kalinka
         if g == '3':
             print("----------Station Overview-----------")
             i = input("Joint Air-Locks: Secure\nPod-Bay Doors: Secured\nPhotovoltaic Arrays: Charging 87% Efficiency\nElectrical: 0 Faults\n HRS Radiators: 38C\nTOTAL FAULTS: 0\n\nroot@HAL9000: ")
         if i == 'Open the pod bay doors please Hal.':
             print("I'm sorry Dave. I'm afraid I can't do that.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if i == 'Do you read me Hal?':
             print("Affirmative, Dave. I read you.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if i == 'Whats the problem Hal?':
             print("I think you know what the problem is just as well as I do.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if i == 'What are you talking about?':
             print("This mission is too important for me to allow you to jeopardize it.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if i == 'What are you talking about, Hal?':
             print("I know that you and Frank were planning to disconnect me and I'm afraid that's something I cannot allow to happen.")
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if f == '3':
             print("------------Quit Session?------------")
             c = 0
@@ -247,8 +300,8 @@ while a == 0:
             print()
         if c == 'n':
             print()
-            d = 'Isp899'
-            e = 'Kalinka'
+            d = Isp899
+            e = Kalinka
         if f == '4':
             d = 0
             e = 0
@@ -260,14 +313,14 @@ while a == 0:
         print()
         print('Welcome, Guest')
         print()
-        f = input("1. System Overview\n2. Quit Session\n3. Logoff\n(1,2,3): ")
+        f = input("1. System Overview\n2. Quit Session\n3. Logoff\n(1-3): ")
         if f == '1':
             print()
             print (time.strftime('%m/%d/%Y %H:%M:%S'))
             print('All Systems Ok')
             print()
-            d = 'Guest'
-            e = 'Guest'
+            d = Guest
+            e = Guest
         if f == '2':
             print("------------Quit Session?------------")
             c = 0
@@ -276,8 +329,8 @@ while a == 0:
             close
         if c == 'n':
             print()
-            d = 'Guest'
-            e = 'Guest'
+            d = Guest
+            e = Guest
         if f == '3':
             d = 0
             e = 0
